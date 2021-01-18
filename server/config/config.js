f09f9e860bf3bdaa7b485fb0f84f7b458a69cd4e
Rainier <rainiermichelena@gmail.com>
2021-01-18 01:32:15 -0500

    Variable de entorno mongo

--- server/config/config.js
+++ server/config/config.js
@@ -19,8 +19,7 @@ let urlDB;
 if (process.env.NODE_ENV === "dev") {
   urlDB = "mongodb://localhost:27017/cafe";
 } else {
-  urlDB =
-    "mongodb+srv://owsmc:gCocn1oLLrVDJBJI@cluster0.oxitr.mongodb.net/cafe";
+  urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
