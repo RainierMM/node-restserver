baa833f5a6ff7e3ec5493b0f031eaecd7301905a
Rainier <rainiermichelena@gmail.com>
2021-01-25 23:03:40 -0500

    Subida de archivos

--- server/middlewares/autenticacion.js
+++ server/middlewares/autenticacion.js
@@ -42,7 +42,31 @@ let verificaAdmin_Role = (req, res, next) => {
   }
 };
 
+/**
+ * VERIFICA Token para imagen
+ */
+
+let verificaToken_Img = (req, res, next) => {
+  let token = req.query.token;
+
+  jwt.verify(token, process.env.SEED, (err, decoded) => {
+    if (err) {
+      return res.status(401).json({
+        ok: false,
+        err: {
+          message: "Token no valido",
+        },
+      });
+    }
+
+    req.usuario = decoded.usuario;
+
+    next();
+  });
+};
+
 module.exports = {
   verificaToken,
   verificaAdmin_Role,
+  verificaToken_Img,
 };
